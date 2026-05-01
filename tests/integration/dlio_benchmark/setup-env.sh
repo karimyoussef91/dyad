9381d9bc306bb2ac62fd8165d175c52a31386a78
Karim Yasser Mohamed Youssef <youssef2@corona82.llnl.gov>
2026-05-01 12:27:06 -0700

    update launch scripts

--- tests/integration/dlio_benchmark/setup-env.sh
+++ tests/integration/dlio_benchmark/setup-env.sh
@@ -4,33 +4,34 @@ module load python/3.9.12
 module load openmpi/4.1.2
 
 # Configurations
-export DLIO_WORKLOAD=dyad_mummi #dyad_unet3d_large # unet3d_base dyad_unet3d dyad_unet3d_small resnet50_base dyad_resnet50 unet3d_base_large mummi_base dyad_mummi
-export NUM_NODES=64
+export DLIO_WORKLOAD=dyad_unet3d #dyad_unet3d_large # unet3d_base dyad_unet3d dyad_unet3d_small resnet50_base dyad_resnet50 unet3d_base_large mummi_base dyad_mummi
+export NUM_NODES=8
 export PPN=8
 export QUEUE=pbatch
-export TIME=$((60))
+export TIME=$((30))
 export BROKERS_PER_NODE=1
 export GENERATE_DATA="0"
 
-export DYAD_INSTALL_PREFIX=/usr/workspace/haridev/dyad/env/spack/.spack-env/view
+export DYAD_INSTALL_PREFIX=/usr/workspace/youssef2/dyad_corona/install
 export DYAD_KVS_NAMESPACE=dyad
 export DYAD_DTL_MODE=UCX
-export DYAD_PATH="/l/ssd/haridev/dyad"
-#export DYAD_PATH=/dev/shm/haridev/dyad
-export GITHUB_WORKSPACE=/usr/workspace/haridev/dyad
-export SPACK_DIR=/usr/workspace/haridev/spack
-export SPACK_ENV=/usr/workspace/haridev/dyad/env/spack
-export PYTHON_ENV=/usr/workspace/haridev/dyad/env/python
-export DLIO_DATA_DIR=/p/lustre1/iopp/dyad/dlio_benchmark/dyad_mummi #  dyad_resnet50 dyad_unet3d_basic
-#export DLIO_DATA_DIR=/p/lustre2/haridev/dyad/dlio_benchmark/dyad_unet3d_basic #  dyad_resnet50
+export DYAD_PATH="/l/ssd/youssef2/dyad_data"
+#export DYAD_PATH=/dev/shm/youssef2/dyad
+export GITHUB_WORKSPACE=/usr/workspace/youssef2/dyad_corona
+export SPACK_DIR=/usr/workspace/youssef2/spack
+export SPACK_ENV=/usr/workspace/youssef2/dyad_corona/env/spack
+export PYTHON_ENV_BASE=/usr/workspace/youssef2/dlio_corona_ompi
+export PYTHON_ENV=/tmp/dlio_corona_ompi
+export DLIO_DATA_DIR=/p/lustre3/youssef2/dlio_data/unet3d_dyad_320 #  dyad_resnet50 dyad_unet3d_basic
+#export DLIO_DATA_DIR=/p/lustre2/youssef2/dyad/dlio_benchmark/dyad_unet3d_basic #  dyad_resnet50
 
 # DLIO Profiler Configurations
-export DFTRACER_ENABLE=1
+export DFTRACER_ENABLE=0
 export DFTRACER_INC_METADATA=1
 export DFTRACER_DATA_DIR=${DLIO_DATA_DIR}:${DYAD_PATH}
-export DFTRACER_LOG_FILE=/usr/workspace/haridev/dyad/tests/integration/dlio_benchmark/profiler/dyad
+export DFTRACER_LOG_FILE=/usr/workspace/youssef2/dyad_corona/tests/integration/dlio_benchmark/profiler/dyad
 
-export DYAD_LOG_DIR=/usr/workspace/haridev/dyad/tests/integration/dlio_benchmark/logs
+export DYAD_LOG_DIR=/usr/workspace/youssef2/dyad_corona/tests/integration/dlio_benchmark/logs
 export DFTRACER_LOG_LEVEL=ERROR
 #export GOTCHA_DEBUG=3
 
@@ -41,9 +42,40 @@ export HDF5_USE_FILE_LOCKING=0
 mkdir -m 775 -p ${DYAD_PATH}
 mkdir -p ${DFTRACER_LOG_FILE}
 
+# Stage the Python environment to node-local /tmp to avoid loading issues from the shared filesystem.
+
+if [ ! -d "${PYTHON_ENV_BASE}" ]; then
+    echo "Base Python environment does not exist: ${PYTHON_ENV_BASE}" >&2
+    exit 1
+fi
+
+export PYTHON_ENV_TAR=/tmp/${USER}/$(basename ${PYTHON_ENV_BASE}).tar
+
+if [ -n "${FLUX_URI:-}" ]; then
+    echo "Running within a Flux allocation; staging Python environment to node-local storage." >&2
+    flux exec -r all bash -c "
+        mkdir -p /tmp/${USER}
+        if [ ! -f '${PYTHON_ENV_TAR}' ]; then
+            tar -C '$(dirname ${PYTHON_ENV_BASE})' -cf '${PYTHON_ENV_TAR}' '$(basename ${PYTHON_ENV_BASE})'
+        fi
+        rm -rf '${PYTHON_ENV}'
+        tar -C '$(dirname ${PYTHON_ENV})' -xf '${PYTHON_ENV_TAR}'
+    "
+else
+    echo "Not running within a Flux allocation; skipping staging of Python environment to node-local storage." >&2
+    bash -c "
+        mkdir -p /tmp/${USER}
+        if [ ! -f '${PYTHON_ENV_TAR}' ]; then
+            tar -C '$(dirname ${PYTHON_ENV_BASE})' -cf '${PYTHON_ENV_TAR}' '$(basename ${PYTHON_ENV_BASE})'
+        fi
+        rm -rf '${PYTHON_ENV}'
+        tar -C '$(dirname ${PYTHON_ENV})' -xf '${PYTHON_ENV_TAR}'
+    "
+fi
+
 # Activate Environments
-. ${SPACK_DIR}/share/spack/setup-env.sh
-spack env activate -p ${SPACK_ENV}
+# . ${SPACK_DIR}/share/spack/setup-env.sh
+# spack env activate -p ${SPACK_ENV}
 source ${PYTHON_ENV}/bin/activate 
 
 # Derived Configurations
